49b55d86df30630f34ce099e4ffa0e7cdff950d6
savemava <savemava@DIN78000380.corp.capgemini.com>
2019-07-04 11:58:03 +0530

    My First commit

---
+++ sample/src/main/java/sample/First.java
@@ -0,0 +1,21 @@
+package sample;
+
+public class First {
+	public static void main(String [] args)
+	{
+		
+		
+		System.out.println("first element of the arrayList: ");
+		
+		
+		
+		System.out.println("First element of the ArrayList: ");
+		
+	
+		}
+			
+			
+			
+	}
+
+
